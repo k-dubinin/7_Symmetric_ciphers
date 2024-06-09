ad7aaed5a8f0facd3f34387d270d36c09c44c97a
k-dubinin <81645875+k-dubinin@users.noreply.github.com>
2024-06-09 23:50:20 +0300

    доп задание 2

--- cbc._сypher.py
+++ cbc._сypher.py
@@ -1,21 +1,27 @@
+import secrets
+
 def xor_bytes(b1, b2):
+    """Выполняет побитовое исключающее ИЛИ для двух байтовых строк."""
     return bytes(x ^ y for x, y in zip(b1, b2))
 
 def pad(text, block_size):
+    """Дополняет текст до кратности размера блока."""
     padding_len = block_size - len(text) % block_size
     padding = bytes([padding_len]) * padding_len
     return text + padding
 
 def unpad(text):
+    """Удаляет дополнение из текста."""
     padding_len = text[-1]
     return text[:-padding_len]
 
 def encrypt_cbc(key, iv, plaintext):
+    """Шифрует текст с помощью режима CBC."""
     block_size = len(key)
     plaintext_padded = pad(plaintext.encode(), block_size)
     blocks = [plaintext_padded[i:i + block_size] for i in range(0, len(plaintext_padded), block_size)]
 
-    prev_cipher_block = iv.encode()  # Преобразуем IV в байтовую строку
+    prev_cipher_block = iv.encode()
     ciphertext = b''
     for block in blocks:
         block_xor_iv = xor_bytes(block, prev_cipher_block)
@@ -25,10 +31,11 @@ def encrypt_cbc(key, iv, plaintext):
     return ciphertext
 
 def decrypt_cbc(key, iv, ciphertext):
+    """Расшифровывает текст с помощью режима CBC."""
     block_size = len(key)
     blocks = [ciphertext[i:i + block_size] for i in range(0, len(ciphertext), block_size)]
 
-    prev_cipher_block = iv.encode()  # Преобразуем IV в байтовую строку
+    prev_cipher_block = iv.encode()
     plaintext_padded = b''
     for block in blocks:
         decrypted_block = xor_bytes(block, key.encode())
@@ -39,12 +46,12 @@ def decrypt_cbc(key, iv, ciphertext):
     return plaintext
 
 # Пример использования
-key_cbc = "mysecretkey"
-iv_cbc = "initialiv123456"
+key_cbc = secrets.token_bytes(16)  # Генерация случайного ключа длиной 16 байт
+iv_cbc = secrets.token_bytes(16)  # Генерация случайного IV длиной 16 байт
 plaintext_cbc = "Hello, World!"
 encrypted_text_cbc = encrypt_cbc(key_cbc, iv_cbc, plaintext_cbc)
 decrypted_text_cbc = decrypt_cbc(key_cbc, iv_cbc, encrypted_text_cbc)
 
 print('Original Text:', plaintext_cbc)
-print('Encrypted Text (CBC):', encrypted_text_cbc)
+print('Encrypted Text (CBC):', encrypted_text_cbc.hex())  # Вывод в шестнадцатеричном формате
 print('Decrypted Text (CBC):', decrypted_text_cbc)
